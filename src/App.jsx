f780b1c8cad26522461eb958fcb3f773f6f8367a
isacarrd <rcpessoaluso@gmail.com>
2026-05-20 23:21:05 -0300

    create: portifolio React iniciado

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react' 
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>Portifolio</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
