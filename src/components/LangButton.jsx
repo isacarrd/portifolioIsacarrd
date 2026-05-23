f2d910d0f4183f07a77cba9438abc004901bd088
isacarrd <rcpessoaluso@gmail.com>
2026-05-22 22:46:59 -0300

    update: criação do componente de troca de linguagem

---
+++ src/components/LangButton.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import Texto from "./Texto";
+import styles from "./LangButton.module.css";
+
+function LangButton() {
+  const { i18n } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const changeLanguage = (e, fallbackLng) => {
+    e.stopPropagation();
+    i18n.changeLanguage(fallbackLng);
+    setIsOpen(false);
+  };
+
+  const isPt = String(i18n.language || "pt")
+    .toLowerCase()
+    .includes("pt");
+
+  return (
+    <div
+      className={`${styles.langContainer} ${isOpen ? styles.open : ""}`}
+      onClick={toggleMenu}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      {/* Idioma Ativo (Sempre visível) */}
+      <div className={styles.activeArea} onClick={toggleMenu}>
+        {/* Idioma */}
+        <div className={styles.flagCont}>
+          <div
+            className={`${styles.flag} ${isPt ? styles.br : styles.us}`}
+          ></div>
+        </div>
+
+        {/* Seta */}
+        <div
+          className={`${styles.arrowContainer} ${
+            isOpen ? styles.arrowContainerTop : styles.arrowContainerDown
+          }`}
+        ></div>
+      </div>
+
+      {/* Opções (Só aparecem quando aberto) */}
+      {isOpen && (
+        <div
+          className={styles.dropdownArea}
+          onClick={(e) => changeLanguage(e, isPt ? "en" : "pt")}
+        >
+          <div
+            className={`${styles.flag} ${isPt ? styles.us : styles.br}`}
+          ></div>
+          <Texto as="span" font="var(--miniHome-mb)" color="var(--branco)">
+            {isPt ? "EN-US" : "PT-BR"}
+          </Texto>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default LangButton;
