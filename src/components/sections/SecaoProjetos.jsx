befb57d2cedd194db876c5638c58774605f077b6
isacarrd <rcpessoaluso@gmail.com>
2026-05-30 19:55:59 -0300

    update: estilização da seção de projetos

--- src/components/sections/SecaoProjetos.jsx
+++ src/components/sections/SecaoProjetos.jsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
 import CardProj from "../ui/CardProj";
+import Texto from "../ui/Texto";
+import styles from "./SecaoProjetos.module.css"
 
 import projMercadinho from "../../assets/images/projects/mercadinhoAragao.png"
 import projHearOfPoets from "../../assets/images/projects//heartOfpoets.png"
@@ -7,13 +9,43 @@ import projHearOfPoets from "../../assets/images/projects//heartOfpoets.png"
 function SecaoProjetos() {
   const { t } = useTranslation();
   return (
-    <div>
-      <CardProj
-        textoPadrao={t("cardProj.proj1")}
-        textoHover={t("cardProj.hover")}
-        bgImage={projHearOfPoets}
-        bgAlt={"Projeto Heart Of Poets"}
-      />
+    <div className={styles.baseProjetos}>
+      <Texto as="h2" color="var(--branco)" font="var(--h2)">
+        {" "}
+        {t("infos.title2")}
+      </Texto>
+      <div className={styles.projs}>
+        <CardProj
+          textoPadrao={t("cardProj.proj1")}
+          textoHover={t("cardProj.hover")}
+          bgImage={projHearOfPoets}
+          bgAlt={"Projeto Heart Of Poets"}
+        />
+        <CardProj
+          textoPadrao={t("cardProj.proj1")}
+          textoHover={t("cardProj.hover")}
+          bgImage={projHearOfPoets}
+          bgAlt={"Projeto Heart Of Poets"}
+        />
+        <CardProj
+          textoPadrao={t("cardProj.proj1")}
+          textoHover={t("cardProj.hover")}
+          bgImage={projHearOfPoets}
+          bgAlt={"Projeto Heart Of Poets"}
+        />
+        <CardProj
+          textoPadrao={t("cardProj.proj1")}
+          textoHover={t("cardProj.hover")}
+          bgImage={projHearOfPoets}
+          bgAlt={"Projeto Heart Of Poets"}
+        />
+        <CardProj
+          textoPadrao={t("cardProj.proj1")}
+          textoHover={t("cardProj.hover")}
+          bgImage={projHearOfPoets}
+          bgAlt={"Projeto Heart Of Poets"}
+        />
+      </div>
     </div>
   );
 }
