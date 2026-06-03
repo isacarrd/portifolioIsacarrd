8a85c0c8c02976eae0099e75746d98ea57d272ec
isacarrd <rcpessoaluso@gmail.com>
2026-06-02 22:57:41 -0300

    update: parâmetro onClick

--- src/components/ui/CardProj.jsx
+++ src/components/ui/CardProj.jsx
@@ -1,34 +1,45 @@
 import { useState } from "react";
 import Texto from "../ui/Texto";
 import styles from "./CardProj.module.css";
+import { t } from "i18next";
 
-function CardProj({ children, textoPadrao, textoHover, bgImage, bgAlt}) {
+function CardProj({
+  children,
+  textoPadrao,
+  textoHover = t("cardProj.hover"),
+  bgImage,
+  bgAlt,
+  onClick,
+}) {
   const [isHovered, setIsHovered] = useState(false);
   return (
-    <a href="#" className={styles.link}>
-      <div
-        className={styles.cardBase}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        {bgImage && (
-          <img src={bgImage} alt={bgAlt} className={styles.imgBanner} />
-        )}
+    <div
+      onClick={onClick}
+      className={styles.cardBase}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{ cursor: "pointer" }}
+      role="button"
+      tabIndex={0}
+    >
+      {bgImage && (
+        <img src={bgImage} alt={bgAlt} className={styles.imgBanner} />
+      )}
 
-        <div className={styles.contCard}>
-          {children}
-          <Texto
-            as="span"
-            font="var(--cardProj)"
-            color="var(--branco)"
-            webkitTextStroke="0.5px var(--laranja)"
-            letterSpacing="var(--padrao-opacity)"
-          >
-            {isHovered ? textoHover : textoPadrao}
-          </Texto>
-        </div>
+      <div className={styles.contCard}>
+        {children}
+        <Texto
+          as="span"
+          font="var(--cardProj)"
+          color="var(--branco)"
+          webkitTextStroke="0.5px var(--laranja)"
+          textAlign="center"
+          letterSpacing="var(--padrao-opacity)"
+        >
+          {isHovered ? textoHover : textoPadrao}
+        </Texto>
       </div>
-    </a>
+    </div>
   );
 }
 
