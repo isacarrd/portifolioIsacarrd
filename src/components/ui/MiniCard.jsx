78f4b1ee9ceeb51a0c915183aec0ea35e1ef77dd
isacarrd <rcpessoaluso@gmail.com>
2026-06-03 21:36:32 -0300

    create: componente de miniCard

---
+++ src/components/ui/MiniCard.jsx
@@ -0,0 +1,20 @@
+import styles from "./MiniCard.module.css";
+import Texto from "./Texto"
+
+function MiniCard({ icoLang, langAlt, langName }) {
+  return (
+    <div className={styles.miniC}>
+      <img src={icoLang} alt={langAlt} className={styles.lang} />
+      <Texto
+        as="span"
+        color="var(--branco)"
+        font="var(--miniCard)"
+        textAlign="center"
+      >
+        {langName}
+      </Texto>
+    </div>
+  )
+}
+
+export default MiniCard;
